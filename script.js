6a03058b4e35221572b3db4e1ac1ee0a46fc8c8a
q-u-e-s-o <163966390+q-u-e-s-o@users.noreply.github.com>
2025-12-12 10:08:56 -0800

    Fix import statement formatting in script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-import {
-  collection,
-  addDoc,
-  serverTimestamp
-} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 window.askForLocation = function () {
   if (!navigator.geolocation) {
